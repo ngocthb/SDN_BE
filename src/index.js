28ded3408f82cdac37f753fdbd6e922d8f9d21c8
DatTien2107 <chytai2012/2gmail.com>
2025-07-07 23:45:17 +0700

    pull dev

--- src/index.js
+++ src/index.js
@@ -2,12 +2,22 @@ const express = require("express");
 const dotenv = require("dotenv");
 const { default: mongoose } = require("mongoose");
 const routes = require("./routes");
+const http = require("http");
+const socketIo = require("socket.io");
 
 dotenv.config();
 const cors = require("cors");
 const bodyParser = require("body-parser");
 
 const app = express();
+const server = http.createServer(app);
+const io = socketIo(server, {
+  cors: {
+    origin: ["http://localhost:3000", "http://localhost:5173"], // Frontend URLs
+    methods: ["GET", "POST"],
+  },
+});
+
 const port = process.env.PORT;
 
 app.use(cors());
@@ -15,6 +25,15 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 routes(app);
+
+// Socket.IO connection handling
+io.on("connection", (socket) => {
+  console.log("User connected:", socket.id);
+
+  socket.on("disconnect", () => {
+    console.log("User disconnected:", socket.id);
+  });
+});
 
 mongoose
   .connect(process.env.MONGO_URL)
@@ -26,6 +45,7 @@ mongoose
     console.log(error);
   });
 
-app.listen(port, () => {
+// Thay app.listen bằng server.listen
+server.listen(port, () => {
   console.log("Server is running on port " + port);
 });
