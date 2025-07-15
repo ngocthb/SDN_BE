0e0bebc96418b2cb94909cc596d7a8492572278e
DatTien2107 <chytai2012/2gmail.com>
2025-07-15 13:44:00 +0700

    fix statistic dashboard

--- src/routes/AdminMembershipRouter.js
+++ src/routes/AdminMembershipRouter.js
@@ -1,13 +1,49 @@
 const express = require("express");
 const AdminRouterMembership = express.Router();
 const membershipController = require("../controller/MembershipController");
-const { authUserMiddleware, authAdminMiddleware } = require("../middleware/authMiddleware");
+const adminMembershipController = require("../controller/AdminMembershipController");
+const {
+  authUserMiddleware,
+  authAdminMiddleware,
+} = require("../middleware/authMiddleware");
 
-AdminRouterMembership.get("/", authUserMiddleware, authAdminMiddleware, membershipController.getAllMemberships);
-AdminRouterMembership.get("/:id", authUserMiddleware, authAdminMiddleware, membershipController.getMembershipByIdAdmin);
-AdminRouterMembership.post("/create", authUserMiddleware, authAdminMiddleware, membershipController.createMembership);
-AdminRouterMembership.put("/update/:id", authUserMiddleware, authAdminMiddleware, membershipController.updateMembership);
-AdminRouterMembership.delete("/delete/:id", authUserMiddleware, authAdminMiddleware, membershipController.deleteMembership);
-AdminRouterMembership.patch("/restore/:id", authUserMiddleware, authAdminMiddleware, membershipController.restoreMembership);
+AdminRouterMembership.get(
+  "/statistics",
+  authUserMiddleware,
+  authAdminMiddleware,
+  adminMembershipController.getMembershipStatistics
+);
+
+AdminRouterMembership.get(
+  "/:id",
+  authUserMiddleware,
+  authAdminMiddleware,
+
+  membershipController.getMembershipByIdAdmin
+);
+AdminRouterMembership.post(
+  "/create",
+  authUserMiddleware,
+  authAdminMiddleware,
+  membershipController.createMembership
+);
+AdminRouterMembership.put(
+  "/update/:id",
+  authUserMiddleware,
+  authAdminMiddleware,
+  membershipController.updateMembership
+);
+AdminRouterMembership.delete(
+  "/delete/:id",
+  authUserMiddleware,
+  authAdminMiddleware,
+  membershipController.deleteMembership
+);
+AdminRouterMembership.patch(
+  "/restore/:id",
+  authUserMiddleware,
+  authAdminMiddleware,
+  membershipController.restoreMembership
+);
 
 module.exports = AdminRouterMembership;
